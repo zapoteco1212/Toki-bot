ffafb129aa87c8bffb88d59908e23107fb34479e
werkito <zapoteco1234567@gmail.com>
2026-09-14 23:01:29 -0600

    Modify owner and pairingNumber arrays
    Updated owner and pairingNumber to include a second entry.

--- settings.js
+++ settings.js
@@ -1,3 +1,3 @@
 global.botName = "Toki-Bot"
-global.owner = ["5217442573779"]
-global.pairingNumber = "5217442573779"
+global.owner = ["5217442573779"] ["5217441863414"]
+global.pairingNumber = "5217442573779" "5217441863414"           
