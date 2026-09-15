0ba8d42194244d8bd5065a0115071b134a6feaf4
Toki <toki@bot.com>
2026-09-14 23:12:14 -0600

    fix syntax pairingNumber

--- settings.js
+++ settings.js
@@ -1,3 +1,8 @@
-global.botName = "Toki-Bot"
-global.owner = ["5217442573779"] ["5217441863414"]
-global.pairingNumber = "5217442573779" "5217441863414"           
+global.owner = ["5217442573779", "5217441863414", "5213121447080"]
+global.botNumber = "5217442573779"
+global.pairingNumber = "5217442573779"
+global.sessionName = "./Sessions/Owner"
+global.botname = "Toki-bot"
+global.db = { data: { settings: {}, chats: {}, users: {} } }
+global.loadDatabase = async () => {}
+global.conns = []
