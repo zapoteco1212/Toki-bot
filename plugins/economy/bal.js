ccf220e99eddce9c5917b5ee0fd41feb2ff54301
werkito <zapoteco1234567@gmail.com>
2026-09-17 21:48:45 -0600

    Refactor balance command handler in bal.js

--- plugins/economy/bal.js
+++ plugins/economy/bal.js
@@ -1,28 +1,15 @@
-export default {
-  command: ['bal','balance','cartera','wallet','coins','dinero'],
-  category: 'economy',
-  run: async ({ msg, sock }) => {
-    let who = msg.sender || msg.key?.participant || msg.key?.remoteJid
-    let id = who
+let handler = async (sock, m, args) => {
+  let who = m.key?.participant || m.key?.remoteJid || m.sender || m.chat
+  let data = global.db?.data?.users?.[who] || global.db?.users?.[who] || {}
 
-    let data = {}
-    try {
-      if (global.db?.data?.users) data = global.db.data.users[id] || global.db.data.users[who] || {}
-      else if (global.db?.users) data = global.db.users[id] || {}
-    } catch {}
+  let coins = data.coins?? data.coin?? data.money?? data.dinero?? data.wallet?? data.limit?? 0
+  let bank = data.bank?? data.banco?? 0
+  let exp = data.exp?? data.xp?? 0
+  let level = data.level?? data.nivel?? 0
 
-    let coins = data.coins?? data.coin?? data.money?? data.dinero?? data.limit?? 0
-    let bank = data.bank?? data.banco?? 0
-    let exp = data.exp?? data.xp?? 0
-    let level = data.level?? data.nivel?? 0
-
-    if (coins === 0 && bank === 0) {
-      coins = data.wallet?? 0
-    }
-
-    let txt = `╭─〔 ✿ 𝗧𝗢𝗞𝗜 - 𝗕𝗔𝗟 ✿ 〕─╮
+  let txt = `╭─〔 ✿ 𝗧𝗢𝗞𝗜 - 𝗕𝗔𝗟 ✿ 〕─╮
 │
-│ ❀ @${id.split('@')[0]}
+│ ❀ @${who.split('@')[0]}
 │
 │ 💰 Cartera: ${coins}
 │ 🏦 Banco: ${bank}
@@ -31,6 +18,9 @@ export default {
 │
 ╰─〔 Toki Bot 〕─╯`
 
-    return await sock.sendMessage(msg.chat, { text: txt, mentions: [id] }, { quoted: msg })
-  }
+  await sock.sendMessage(m.chat, { text: txt, mentions: [who] }, { quoted: m })
 }
+
+handler.command = ['bal','balance','cartera','wallet','coins','dinero']
+handler.category = 'economia'
+export default handler
