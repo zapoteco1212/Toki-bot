1613ab750c0449e8d4a930df89a0f45e101c6b7c
werkito <zapoteco1234567@gmail.com>
2026-09-17 21:32:54 -0600

    Enhance balance command with additional features

--- plugins/economy/bal.js
+++ plugins/economy/bal.js
@@ -1,40 +1,36 @@
-import fs from 'fs'
-let db
-for (let p of ["../../lib/db.js","../../lib/database.js","../../src/db.js","../../database.js","../../db.js"]) {
-  try { db = (await import(p)).default; break } catch {}
-}
-
 export default {
-  command: ['bal','balance','cartera','wallet','coins'],
+  command: ['bal','balance','cartera','wallet','coins','dinero'],
   category: 'economy',
   run: async ({ msg, sock }) => {
-    const jid = msg.sender
-    let user = { coins: 0, bank: 0, exp: 0 }
+    let who = msg.sender || msg.key?.participant || msg.key?.remoteJid
+    let id = who
+
+    let data = {}
     try {
-      if (global.db && global.db.data && global.db.data.users && global.db.data.users[jid]) {
-        user = global.db.data.users[jid]
-      } else if (db && db.getUser) {
-        user = await db.getUser(jid) || user
-      } else if (db && db.get) {
-        user = await db.get(jid) || user
-      }
+      if (global.db?.data?.users) data = global.db.data.users[id] || global.db.data.users[who] || {}
+      else if (global.db?.users) data = global.db.users[id] || {}
     } catch {}
 
-    let coins = user.coins || user.money || user.limit || 0
-    let bank = user.bank || user.banco || 0
-    let exp = user.exp || user.xp || 0
+    let coins = data.coins?? data.coin?? data.money?? data.dinero?? data.limit?? 0
+    let bank = data.bank?? data.banco?? 0
+    let exp = data.exp?? data.xp?? 0
+    let level = data.level?? data.nivel?? 0
+
+    if (coins === 0 && bank === 0) {
+      coins = data.wallet?? 0
+    }
 
     let txt = `╭─〔 ✿ 𝗧𝗢𝗞𝗜 - 𝗕𝗔𝗟 ✿ 〕─╮
 │
-│ ❀ Usuario: @${jid.split('@')[0]}
+│ ❀ @${id.split('@')[0]}
 │
 │ 💰 Cartera: ${coins}
 │ 🏦 Banco: ${bank}
-│ ✨ Total: ${coins + bank}
-│ ⭐ Exp: ${exp}
+│ 💎 Total: ${coins + bank}
+│ ⭐ Nivel: ${level} | Exp: ${exp}
 │
-╰─〔 Toki Bot Economy 〕─╯`
+╰─〔 Toki Bot 〕─╯`
 
-    return sock.sendMessage(msg.chat, { text: txt, mentions: [jid] }, { quoted: msg })
+    return await sock.sendMessage(msg.chat, { text: txt, mentions: [id] }, { quoted: msg })
   }
-          }
+}
