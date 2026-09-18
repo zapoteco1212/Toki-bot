c31683d5c07f3d1987bed5be633f343c0dae4b4f
werkito <zapoteco1234567@gmail.com>
2026-09-18 00:23:17 -0600

    Add bot command to toggle group bot status

---
+++ cmds/grupo/bot.js
@@ -0,0 +1,24 @@
+// cmds/grupo/bot.js
+export default {
+  command: ['bot'],
+  category: 'grupo',
+  isAdmin: true,
+  run: async (client, m, args) => {
+    const chat = global.db.data.chats[m.chat]
+    const estado = chat.isBanned?? false
+
+    if (args[0] === 'off') {
+      if (estado) return m.reply('《✧》 El *Bot* ya estaba *desactivado* en este grupo.')
+      chat.isBanned = true
+      return m.reply(`《✧》 Has *Desactivado* a *${global.db.data.settings[client.user.id.split(':')[0] + "@s.whatsapp.net"].namebot}* en este grupo.`)
+    }
+
+    if (args[0] === 'on') {
+      if (!estado) return m.reply(`《✧》 *${global.db.data.settings[client.user.id.split(':')[0] + "@s.whatsapp.net"].namebot}* ya estaba *activado* en este grupo.`)
+      chat.isBanned = false
+      return m.reply(`《✧》 Has *Activado* a *${global.db.data.settings[client.user.id.split(':')[0] + "@s.whatsapp.net"].namebot}* en este grupo.`)
+    }
+
+    return m.reply(`*✿ Estado de ${global.db.data.settings[client.user.id.split(':')[0] + "@s.whatsapp.net"].namebot} (｡•́‿•̀｡)*\n✐ *Actual ›* ${estado? '✗ Desactivado' : '✓ Activado'}\n\n✎ Puedes cambiarlo con:\n> ● _Activar ›_ *bot on*\n> ● _Desactivar ›_ *bot off*`)
+  },
+};
