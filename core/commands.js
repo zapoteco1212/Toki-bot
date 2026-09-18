bbe9f23ad1940bf3a76fb96edc353673f1050bb1
werkito <zapoteco1234567@gmail.com>
2026-09-18 00:42:22 -0600

    Add command menus for various functionalities

---
+++ core/commands.js
@@ -0,0 +1,126 @@
+
+export const bodyMenu = `> 𖧧 ¡Hola! *@$sender*, Soy *$namebot*, Aquí tienes la lista de comandos$cat
+
+༺═━━━━━✦❖✦━━━━━═༻
+❖ *ᴅᴇᴠᴇʟᴏᴘᴇʀ ::* $owner
+✦ *ᴛʏᴘᴇ ::* $botType
+❖ *ᴠᴇʀsɪᴏɴ ::* ^3.0 - Latest
+✦ *sʏsᴛᴇᴍ/ᴏᴘʀ ::* $device
+❖ *ᴛɪᴍᴇ ::* $tiempo, $tempo
+✦ *ᴜsᴇʀs ::* $users
+❖ *ᴜʀʟ ::* $link
+༺═━━━━━✦❖✦━━━━━═༻
+> Vincula un *Socket* con tu número utilizando *$prefixqr* o *$prefixcode*.
+‧꒷︶꒷꒥꒷‧₊˚꒷︶꒷꒥꒷︶꒷˚₊‧꒷꒥꒷︶꒷‧`
+
+export const menuObject = {
+economia: `༺═────────────═༻
+✧･ﾟ: *✧ 𝐄𝐂𝐎𝐍𝐎𝐌𝐘 ✧* :ﾟ･✧
+༺═────────────═༻
+> ✐ Comandos de Economía
+✧ *$prefix...*
+> ...
+✧ *$prefix...*
+> ...
+✧ *$prefix...*
+> ...
+✧ *$prefix...*
+> ...
+✧ *$prefix...*
+> ...
+༺═━━━━━✦❖✦━━━━━═༻`,
+
+gacha: `༺═────────────═༻
+✧･ﾟ: *✧ GACHA ✧* :ﾟ･✧
+༺═────────────═༻
+> ✐ Comandos de Gacha
+✧ *$prefix...*
+> ...
+✧ *$prefix...*
+> ...
+✧ *$prefix...*
+> ...
+༺═━━━━━✦❖✦━━━━━═༻`,
+
+downloads: `༺═────────────═༻
+✧･ﾟ: *✧ DOWNLOADS ✧* :ﾟ･✧
+༺═────────────═༻
+> ✐ Comandos de Descargas
+✧ *$prefix...*
+> ...
+✧ *$prefix...*
+> ...
+✧ *$prefix...*
+> ...
+༺═━━━━━✦❖✦━━━━━═༻`,
+
+profile: `༺═────────────═༻
+✧･ﾟ: *✧ PROFILE ✧* :ﾟ･✧
+༺═────────────═༻
+> ✐ Comandos de Perfil
+✧ *$prefix...*
+> ...
+✧ *$prefix...*
+> ...
+༺═━━━━━✦❖✦━━━━━═༻`,
+
+sockets: `༺═────────────═༻
+✧･ﾟ: *✧ SOCKETS ✧* :ﾟ･✧
+༺═────────────═༻
+> ✐ Comandos de Sockets
+✧ *$prefix...*
+> ...
+✧ *$prefix...*
+> ...
+༺═━━━━━✦❖✦━━━━━═༻`,
+
+stickers: `༺═────────────═༻
+✧･ﾟ: *✧ STICKERS ✧* :ﾟ･✧
+༺═────────────═༻
+> ✐ Comandos de Stickers
+✧ *$prefix...*
+> ...
+✧ *$prefix...*
+> ...
+༺═━━━━━✦❖✦━━━━━═༻`,
+
+utils: `༺═────────────═༻
+✧･ﾟ: *✧ UTILS ✧* :ﾟ･✧
+༺═────────────═༻
+> ✐ Comandos de Utils
+✧ *$prefix...*
+> ...
+✧ *$prefix...*
+> ...
+༺═━━━━━✦❖✦━━━━━═༻`,
+
+grupo: `༺═────────────═༻
+✧･ﾟ: *✧ GROUPS ✧* :ﾟ･✧
+༺═────────────═༻
+> ✐ Comandos de Grupos
+✧ *$prefix...*
+> ...
+✧ *$prefix...*
+> ...
+༺═━━━━━✦❖✦━━━━━═༻`,
+
+nsfw: `༺═────────────═༻
+✧･ﾟ: *✧ NSFW ✧* :ﾟ･✧
+༺═────────────═༻
+> ✐ Comandos NSFW
+✧ *$prefix...*
+> ...
+✧ *$prefix...*
+> ...
+༺═━━━━━✦❖✦━━━━━═༻`,
+
+anime: `༺═────────────═༻
+✧･ﾟ: *✧ ANIME ✧* :ﾟ･✧
+༺═────────────═༻
+> ✐ Comandos de Anime
+✧ *$prefix...*
+> ...
+✧ *$prefix...*
+> ...
+༺═━━━━━✦❖✦━━━━━═༻`
+}
