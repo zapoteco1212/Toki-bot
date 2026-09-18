e8ad8b1e77ee584910f0babd5786c2c104f25d95
werkito <zapoteco1234567@gmail.com>
2026-09-17 19:12:12 -0600

    Add 'speed' command to ping plugin

--- plugins/ping.js
+++ plugins/ping.js
@@ -1,7 +1,26 @@
 export default {
-  command: ['ping','p'],
+  command: ['ping', 'p', 'speed'],
+  help: ['ping'],
+  tags: ['info'],
+  desc: 'Muestra la velocidad del Bot.',
+
   run: async (client, m) => {
     const start = Date.now()
-    await client.sendMessage(m.chat, { text: `✿ Pong! ${Date.now()-start}ms` }, { quoted: m })
+    const msg = await client.sendMessage(m.chat, { text: '✿ *Calculando...* ✿' }, { quoted: m })
+    
+    const end = Date.now() - start
+    const uptime = Math.floor(process.uptime())
+    const h = Math.floor(uptime / 3600)
+    const min = Math.floor((uptime % 3600) / 60)
+    const s = uptime % 60
+
+    let txt = `> *¡Pong!* 🏓\n\n`
+    txt += `⌒࣪᷼⏜͡  ۪  ࿚ꨪᰰ࿙  ࣭࣪⢏࣭۟⢢࣭ׄ᎐፝֟᎐࣭ׄ⡔࣭۟⡹࣭ׄ  ࿚ꨪᰰ࿙  ۪  ͡⏜ׄ᷼⌒\n\n`
+    txt += `: ̗̀❖ *ᴠᴇʟᴏᴄɪᴅᴀᴅ ::* ${end}ms\n`
+    txt += `: ̗̀❖ *ᴀᴄᴛɪᴠᴏ ::* ${h}h ${min}m ${s}s\n`
+    txt += `: ̗̀❖ *ʙᴏᴛ ::* Toki-Bot ✿\n\n`
+    txt += `⌒࣪᷼⏜͡  ۪  ࿚ꨪᰰ࿙  ࣭࣪⢏࣭۟⢢࣭ׄ᎐፝֟᎐࣭ׄ⡔࣭۟⡹࣭ׄ  ࿚ꨪᰰ࿙  ۪  ͡⏜ׄ᷼⌒`
+
+    await client.sendMessage(m.chat, { text: txt, edit: msg.key })
   }
 }
