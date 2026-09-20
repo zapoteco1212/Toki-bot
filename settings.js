7a4520977e78dea87af784e0926ae55cfcecf99e
werkito <zapoteco1234567@gmail.com>
2026-09-20 13:38:53 -0600

    Clean up owner arrays by removing empty entries
    Removed empty strings from mainOwners and extraOwners arrays.

--- settings.js
+++ settings.js
@@ -7,28 +7,9 @@ const botOwner = ['5217551011162']
 const mainOwners = [
 '5217442573779', 
 '5217441863414', 
-'', 
-'', 
-'', 
-'',
-'', 
 ]
 
 const extraOwners = [
-'', 
-'', 
-'', 
-'',
-'', 
-'', 
-'', 
-'', 
-', 
-'', 
-'', 
-'', 
-'', 
-'', 
 ]
 
 global.owner = [...botOwner, ...mainOwners, ...extraOwners]
